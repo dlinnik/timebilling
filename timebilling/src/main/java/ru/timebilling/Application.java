07c0ad392b974a81fc9e63993b6bffe8f51ca716
vshmelev <vshmelev@gmail.com>
2014-03-07 16:58:32 +0300

    test commit

--- timebilling/src/main/java/ru/timebilling/Application.java
+++ timebilling/src/main/java/ru/timebilling/Application.java
@@ -12,6 +12,6 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
-    	ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);    	
+    	SpringApplication.run(Application.class, args);    	
     }
 }
