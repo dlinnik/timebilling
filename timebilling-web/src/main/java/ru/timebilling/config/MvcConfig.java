cefb2c3d9c1487d5f11db778caeee27d7018225e
vshmelev <vshmelev@gmail.com>
2014-03-17 18:28:42 +0300

    fixed

--- timebilling-web/src/main/java/ru/timebilling/config/MvcConfig.java
+++ timebilling-web/src/main/java/ru/timebilling/config/MvcConfig.java
@@ -1,7 +1,6 @@
 package ru.timebilling.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
